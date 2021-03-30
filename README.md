69ce26cacda3e5edd1fa177695f1ba6d953a0dd3
Jarkko Tenhola <jarkko.tenhola@gmail.com>
2021-03-30 13:20:41 +0300

    Create the basic game functionalities, moving ship and firing bullets.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Alien invasion
+
+## About
+This repository contains an instance of the famous "Alien Invasion!" computer game. This project follows almost exactly the instructions found in the book "Python Crash Course, 2nd Edition" by Eric Matthes.
+
+## Dependencies
+This game uses the module [PyGame] (https://www.pygame.org). It's a super useful package for python for constructing simple games like this.
+
+## Playing the game
+You can run your game through the terminal/command line. If you have python 3 installed on your computer and in your global PATH environment, just execute the command `python alien_invasion.py`in your terminal/command line. If it doesn't work, try using `python3 alien_invasion.py` instead.
---
+++ ship.py
@@ -0,0 +1,48 @@
+import pygame
+
+class Ship:
+    """
+    A class to manage the ship.
+    """
+
+    def __init__(self, ai_game) -> None:
+        """
+        Initialize the ship and set its starting position.
+        """
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Load the image and get its rect.
+        self.image = pygame.image.load("./images/ship.bmp")
+        self.rect = self.image.get_rect()
+
+        # Start each ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """
+        Update the ship's position based on the movement flag.
+        """
+        # Update the ship's x value, not the rect.
+        # Rect accepts only integer values.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """
+        Draw the ship and its current location.
+        """
+        self.screen.blit(self.image, self.rect)
